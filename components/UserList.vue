9c019e885714999a36c192bc5ecc5f482066a6af
Hojiakbar <namozovhojiajbar6@gmail.com>
2026-09-13 17:52:03 +0500

    CRM loyiha tayyor

---
+++ components/UserList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="user-list">
+    <h2>Foydalanuvchilar ro'yxati</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserList",
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Ali Valiyev", email: "ali@mail.com" },
+        { id: 2, name: "Otabek Toshmatov", email: "otabek@mail.com" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.user-list {
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
